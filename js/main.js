fcf77333f36c1b8ad2edd21f62d400f2934554b9
Tkachuk Ilya <tim@MacBook-Pro-Tkachuk.local>
2017-11-16 01:29:41 +0200

    add awesome novacancy neon animations

--- js/main.js
+++ js/main.js
@@ -2,16 +2,33 @@
 $(document).ready(function(){
 
 	var main = function(colors, animations){
+
 		$('[data-toggle="popover"]').popover();
 		$('[data-toggle="tooltip"]').tooltip();
 
-    var typed = new Typed('#app-greetings', {
-      strings: ['Ilja Tkachuk^500\naka timCF'],
+    var glow_color  = '#ffffff'
+    var glow_blur   = ['0 0 15px #ffffff', '0 0 10px #008000', '0 0 5px #0000ff']
+
+    new Typed('#app-myname', {
+      strings:    ['Ilja Tkachuk^500\naka timCF'],
       showCursor: true,
       cursorChar: "▐",
-      typeSpeed: 30
+      typeSpeed:  30,
+      onComplete: function(){
+        $('#app-myname').novacancy({
+          color:  glow_color,
+          glow:   glow_blur,
+          off:    2,
+          blink:  0,
+          autoOn: true,
+        });
+        $(".typed-cursor").novacancy({
+          color:  glow_color,
+          glow:   glow_blur,
+          autoOn: false,
+        });
+      }
     });
-
 	};
 
 	$.get('/data/colors.yml').done(function(colors){
